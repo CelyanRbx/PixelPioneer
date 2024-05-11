a7cc70bd2061fe8267d257d46233f7fc9f4c9c5c
CelyanRbx <glmtjulie@gmail.com>
2024-05-11 20:38:25 +0200

    Update

--- src/main/java/fr/celyanrbx/lolomobs/datagen/DataGenerators.java
+++ src/main/java/fr/celyanrbx/lolomobs/datagen/DataGenerators.java
@@ -29,4 +29,5 @@ public class DataGenerators {
         ModBlockTagGenerator blockTagGenerator = generator.addProvider(event.includeServer(),
                 new ModBlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(), new ModItemTagGenerator(packOutput, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));
+    }
 }
