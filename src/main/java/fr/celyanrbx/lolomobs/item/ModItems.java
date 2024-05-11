a0739f64636d056e51ac642d39719a6c5eda6a56
CelyanRbx <glmtjulie@gmail.com>
2024-05-11 23:33:27 +0200

    Update

--- src/main/java/fr/celyanrbx/lolomobs/item/ModItems.java
+++ src/main/java/fr/celyanrbx/lolomobs/item/ModItems.java
@@ -23,10 +23,10 @@ public class ModItems {
             () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
-            () -> new Item(new Item.Properties().stacksTo(1)));
+            () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
 
     public static final RegistryObject<Item> SAPPHIRE_STAFF = ITEMS.register("sapphire_staff",
-            () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
             () -> new FuelItem(new Item.Properties(), 400));
