066443af75e952cd322a5cf3136340e1b14825e3
CelyanRbx <glmtjulie@gmail.com>
2024-05-11 20:37:09 +0200

    Update

--- src/main/java/fr/celyanrbx/lolomobs/datagen/DataGenerators.java
+++ src/main/java/fr/celyanrbx/lolomobs/datagen/DataGenerators.java
@@ -7,7 +7,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.concurrent.CompletableFuture;
 
