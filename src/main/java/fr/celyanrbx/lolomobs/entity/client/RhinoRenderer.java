4ceb58aea2388e4554ca35c7dbea89c8e5339319
CelyanRbx <glmtjulie@gmail.com>
2024-05-12 18:33:53 +0200

    Add Rhino & Rhino Spawn Egg

---
+++ src/main/java/fr/celyanrbx/lolomobs/entity/client/RhinoRenderer.java
@@ -0,0 +1,30 @@
+package fr.celyanrbx.lolomobs.entity.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import fr.celyanrbx.lolomobs.LoloMobs;
+import fr.celyanrbx.lolomobs.entity.custom.RhinoEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+
+public class RhinoRenderer extends MobRenderer<RhinoEntity, RhinoModel<RhinoEntity>> {
+    public RhinoRenderer(EntityRendererProvider.Context pContext) {
+        super(pContext, new RhinoModel<>(pContext.bakeLayer(ModModelLayers.RHINO_LAYER)), 2f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(RhinoEntity pEntity) {
+        return new ResourceLocation(LoloMobs.MOD_ID, "textures/entity/rhino.png");
+    }
+
+    @Override
+    public void render(RhinoEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack,
+                       MultiBufferSource pBuffer, int pPackedLight) {
+        if(pEntity.isBaby()) {
+            pMatrixStack.scale(0.5f, 0.5f, 0.5f);
+        }
+
+        super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
+    }
+}
