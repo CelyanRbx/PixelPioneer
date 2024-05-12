d615136c99f399c7d43c7b004f690f36c4d4afa8
CelyanRbx <glmtjulie@gmail.com>
2024-05-12 03:11:40 +0200

    Add Loot Modifiers

---
+++ src/main/java/fr/celyanrbx/lolomobs/datagen/ModGlobalLootModifiersProvider.java
@@ -0,0 +1,37 @@
+package fr.celyanrbx.lolomobs.datagen;
+
+import fr.celyanrbx.lolomobs.LoloMobs;
+import fr.celyanrbx.lolomobs.item.ModItems;
+import fr.celyanrbx.lolomobs.loot.AddItemModifier;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootTableIdCondition;
+
+public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
+    public ModGlobalLootModifiersProvider(PackOutput output) {
+        super(output, LoloMobs.MOD_ID);
+    }
+
+    @Override
+    protected void start() {
+        add("pine_cone_from_grass", new AddItemModifier(new LootItemCondition[] {
+                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS).build(),
+                LootItemRandomChanceCondition.randomChance(0.35f).build()}, ModItems.PINE_CONE.get()));
+
+        add("pine_cone_from_creeper", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/creeper")).build() }, ModItems.PINE_CONE.get()));
+
+        add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.METAL_DETECTOR.get()));
+
+
+
+
+
+    }
+}
