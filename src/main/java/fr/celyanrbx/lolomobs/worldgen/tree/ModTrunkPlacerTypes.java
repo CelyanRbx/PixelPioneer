1f5d6195cb523e74d479061e19ebe5d994511e8a
CelyanRbx <glmtjulie@gmail.com>
2024-05-13 17:21:54 +0200

    Add Trunk Placer

---
+++ src/main/java/fr/celyanrbx/lolomobs/worldgen/tree/ModTrunkPlacerTypes.java
@@ -0,0 +1,21 @@
+package fr.celyanrbx.lolomobs.worldgen.tree;
+
+import fr.celyanrbx.lolomobs.LoloMobs;
+import fr.celyanrbx.lolomobs.worldgen.tree.custom.PineTrunkPlacer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacerType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModTrunkPlacerTypes {
+    public static final DeferredRegister<TrunkPlacerType<?>> TRUNK_PLACER =
+            DeferredRegister.create(Registries.TRUNK_PLACER_TYPE, LoloMobs.MOD_ID);
+
+    public static final RegistryObject<TrunkPlacerType<PineTrunkPlacer>> PINE_TRUNK_PLACER =
+            TRUNK_PLACER.register("pine_trunk_placer", () -> new TrunkPlacerType<>(PineTrunkPlacer.CODEC));
+
+    public static void register(IEventBus eventBus) {
+        TRUNK_PLACER.register(eventBus);
+    }
+}
