267e74988d12068fb7521aed38d23969281b6e96
CelyanRbx <glmtjulie@gmail.com>
2024-05-13 13:21:44 +0200

    Add Ore Generation

---
+++ src/main/java/fr/celyanrbx/lolomobs/datagen/ModWorldGenProvider.java
@@ -0,0 +1,27 @@
+package fr.celyanrbx.lolomobs.datagen;
+
+import fr.celyanrbx.lolomobs.LoloMobs;
+import fr.celyanrbx.lolomobs.worldgen.ModBiomeModifiers;
+import fr.celyanrbx.lolomobs.worldgen.ModConfiguredFeatures;
+import fr.celyanrbx.lolomobs.worldgen.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(LoloMobs.MOD_ID));
+    }
+}
+
