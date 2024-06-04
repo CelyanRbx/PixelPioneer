1182be17fdbea94446acba1c1a40a24318f4d097
CelyanRbx <glmtjulie@gmail.com>
2024-06-04 11:40:19 +0200

    Update

---
+++ src/main/java/fr/celyanrbx/pixelpioneer/compat/CompatConstants.java
@@ -0,0 +1,11 @@
+package fr.celyanrbx.pixelpioneer.compat;
+
+public class CompatConstants {
+    public static final String CRAFTTWEAKER = "crafttweaker";
+    public static final String CURIOS = "curios";
+    public static final String IRONFURNACES = "ironfurnaces";
+    public static final String JEITWEAKER = "jeitweaker";
+
+    public CompatConstants() {
+    }
+}
